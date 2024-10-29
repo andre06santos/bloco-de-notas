4d60880898797a7197cb83fe49d0f051e7530d84
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-10-29 11:06:27 -0300

    :recycle: retira importação desnecessaria

--- src/pages/edit-page.tsx
+++ src/pages/edit-page.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const EditPage = () => {
   return (
     <div>edit-page</div>
--- src/pages/notes-page.tsx
+++ src/pages/notes-page.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const NotesPage = () => {
   return (
     <div>notes-page</div>
