b5865c586fbe9ecc76b5621265d698e27393e941
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-10-29 13:09:17 -0300

    :recycle: feat/issue #9 testa textos grandes e altera para tag main

--- src/pages/notes-page.tsx
+++ src/pages/notes-page.tsx
@@ -4,8 +4,8 @@ const NotesPage = () => {
   const data = [
     {
       id: 1,
-      title: "Text 1",
-      text: "Description 1",
+      title: "Text kdsfajk fh asjkfh shdafjk sdhf jkds",
+      text: "Description asjfh jdsahf kjdsah fjkh sajfhSKLHFSJA FHJKDSA FHJKSHD FKJHashd ksajfk sdakf kasdh fljahe kdns khfdsjka fhjsda fhuweah fuiehafjksdn fjkshdfjlhsdak fjskdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa hgj h",
     },
     {
       id: 2,
@@ -22,14 +22,34 @@ const NotesPage = () => {
         title: "Text 2",
         text: "Description 2",
       },
+      {
+        id: 1,
+        title: "Text 1",
+        text: "Description 1",
+      },
+      {
+        id: 2,
+        title: "Text 2",
+        text: "Description 2",
+      },
+      {
+        id: 1,
+        title: "Text 1",
+        text: "Description 1",
+      },
+      {
+        id: 2,
+        title: "Text 2",
+        text: "Description 2",
+      },
   ];
 
   return (
-    <div className="notes">
+    <main className="notes">
       {data.map((note) => (
         <Note key={note.id} note={note} />
       ))}
-    </div>
+    </main>
   );
 };
 
