363b96be8362929fdb27eca7f05356e953e5c1c3
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-10-29 12:02:56 -0300

    :sparkles: cria pagina de notas

--- src/pages/notes-page.tsx
+++ src/pages/notes-page.tsx
@@ -1,7 +1,36 @@
-const NotesPage = () => {
-  return (
-    <div>notes-page</div>
-  )
-}
+import { Note } from "../ui/note";
 
-export {NotesPage}
+const NotesPage = () => {
+  const data = [
+    {
+      id: 1,
+      title: "Text 1",
+      text: "Description 1",
+    },
+    {
+      id: 2,
+      title: "Text 2",
+      text: "Description 2",
+    },
+    {
+        id: 1,
+        title: "Text 1",
+        text: "Description 1",
+      },
+      {
+        id: 2,
+        title: "Text 2",
+        text: "Description 2",
+      },
+  ];
+
+  return (
+    <div className="notes">
+      {data.map((note) => (
+        <Note key={note.id} note={note} />
+      ))}
+    </div>
+  );
+};
+
+export { NotesPage };
