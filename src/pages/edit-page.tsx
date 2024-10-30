c7e2d7154e4250158d3aafc297ffb994dcd2d2cd
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-10-30 09:31:12 -0300

    :sparkles: feat/issue #9 adiciona pagina de edit

--- src/pages/edit-page.tsx
+++ src/pages/edit-page.tsx
@@ -1,7 +1,30 @@
 const EditPage = () => {
   return (
-    <main>edit-page</main>
-  )
-}
+    <main>
+      <h1>Edite a nota</h1>
 
-export {EditPage}
+      <form action="post" className="form">
+        <label htmlFor="title">
+          Título
+          <input type="text" name="title" id="title" className="form__title" />
+        </label>
+
+        <label htmlFor="text">
+          Descrição
+          <textarea name="text" id="text" className="form__text"></textarea>
+        </label>
+
+        <div className="buttons">
+          <input
+            type="button"
+            value="CANCELAR"
+            className="form__button cancel"
+          />
+          <input type="button" value="CRIAR" className="form__button create" />
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export { EditPage };
