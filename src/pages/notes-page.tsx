5c779dcabd445d32437f0702b09d58237c8a3968
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-10-30 08:51:19 -0300

    :recycle: mudança de lugar dos dados

--- src/pages/notes-page.tsx
+++ src/pages/notes-page.tsx
@@ -1,56 +1,56 @@
 import { Note } from "../ui/note";
 
 const NotesPage = () => {
-  const data = [
-    {
-      id: 1,
-      title: "Text kdsfajk fh asjkfh shdafjk sdhf jkds",
-      text: "Description asjfh jdsahf kjdsah fjkh sajfhSKLHFSJA FHJKDSA FHJKSHD FKJHashd ksajfk sdakf kasdh fljahe kdns khfdsjka fhjsda fhuweah fuiehafjksdn fjkshdfjlhsdak fjskdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa hgj h",
-    },
-    {
-      id: 2,
-      title: "Text 2",
-      text: "Description 2",
-    },
-    {
-        id: 1,
-        title: "Text 1",
-        text: "Description 1",
-      },
-      {
-        id: 2,
-        title: "Text 2",
-        text: "Description 2",
-      },
-      {
-        id: 1,
-        title: "Text 1",
-        text: "Description 1",
-      },
-      {
-        id: 2,
-        title: "Text 2",
-        text: "Description 2",
-      },
-      {
-        id: 1,
-        title: "Text 1",
-        text: "Description 1",
-      },
-      {
-        id: 2,
-        title: "Text 2",
-        text: "Description 2",
-      },
-  ];
-
   return (
     <main className="notes">
       {data.map((note) => (
-        <Note key={note.id} note={note} />
+        <Note key={note.id} title={note.title} text={note.text}/>
       ))}
     </main>
   );
 };
+
+const data = [
+  {
+    id: 1,
+    title: "Text kdsfajk fh asjkfh shdafjk sdhf jkds",
+    text: "Description asjfh jdsahf kjdsah fjkh sajfhSKLHFSJA FHJKDSA FHJKSHD FKJHashd ksajfk sdakf kasdh fljahe kdns khfdsjka fhjsda fhuweah fuiehafjksdn fjkshdfjlhsdak fjskdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa hgj h",
+  },
+  {
+    id: 2,
+    title: "Text 2",
+    text: "Description 2",
+  },
+  {
+    id: 1,
+    title: "Text 1",
+    text: "Description 1",
+  },
+  {
+    id: 2,
+    title: "Text 2",
+    text: "Description 2",
+  },
+  {
+    id: 1,
+    title: "Text 1",
+    text: "Description 1",
+  },
+  {
+    id: 2,
+    title: "Text 2",
+    text: "Description 2",
+  },
+  {
+    id: 1,
+    title: "Text 1",
+    text: "Description 1",
+  },
+  {
+    id: 2,
+    title: "Text 2",
+    text: "Description 2",
+  },
+];
 
 export { NotesPage };
