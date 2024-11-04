924dcb2ff5cc7e27102d9bf4bd530098bf32fca3
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-04 09:32:38 -0300

    :sparkles: feat/issue #16 adiciona rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Layout } from "./ui/layout";
+import { NotesPage } from "./pages/notes-page";
+import { CreatePage } from "./pages/create-page";
+import { EditPage } from "./pages/edit-page";
 
 function App() {
-  return <p>Qualquer coisa</p>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<NotesPage />} />
+            <Route path="/create" element={<CreatePage />} />
+            <Route path="/edit" element={<EditPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+
+  )
+  
 }
 
 export default App;
