d86f7227b91034701a03ee4d0a02fbcc33f0db40
Thalisson Moura <thalissonmoura138@gmail.com>
2024-11-04 11:53:53 -0300

    resolve conflito cherry-pick

---
+++ src/pages/notes-page/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { Note } from "../../ui/note";
+
+const NotesPage = () => {
+  return (
+    <div className="container-notas">
+      <Note />
+    </div>
+  );
+};
+
+export { NotesPage };
