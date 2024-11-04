34fd21a4e3f565a3a41fa7e6efde47f3d5e636aa
Thalisson Moura <thalissonmoura138@gmail.com>
2024-11-04 11:53:53 -0300

    remove o estilo da cor do texto para visualizacao do dom

--- src/pages/notes-page/index.tsx
+++ src/pages/notes-page/index.tsx
@@ -1,7 +1,12 @@
 import "./styles.css";
+import { Note } from "../../ui/note";
 
 const NotesPage = () => {
-  return <div className="container-notas"></div>;
+  return (
+    <div className="container-notas">
+      <Note />
+    </div>
+  );
 };
 
 export { NotesPage };
