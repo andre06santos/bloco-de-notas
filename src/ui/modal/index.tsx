105504965ac3c5ab7525b6823b30a2a0f9a65f76
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-05 10:38:09 -0300

    :sparkles: feat/issue #29 adiciona fundo transparente ao modal

--- src/ui/modal/index.tsx
+++ src/ui/modal/index.tsx
@@ -1,16 +1,18 @@
-import "./styles.css"
+import "./styles.css";
 
 const Modal = () => {
   return (
-    <div className="modal">
+    <div className="modal__bg">
+      <div className="modal">
         <p className="modal__text">Tem certeza que deseja excluir esta nota?</p>
 
         <div className="modal__buttons">
-            <button className="modal__buttons__excluir">Excluir</button>
-            <button className="modal__buttons__cancelar">Cancelar</button>
+          <button className="modal__buttons__excluir">Excluir</button>
+          <button className="modal__buttons__cancelar">Cancelar</button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export {Modal}
+export { Modal };
