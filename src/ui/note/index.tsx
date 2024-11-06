30a144f44b151d2db253a20591c1ad76c69ce5b3
Thalisson <thalissonmoura138@gmail.com>
2024-11-06 15:44:25 -0300

    refactor/issue #33 remove "s" de tag div

--- src/ui/note/index.tsx
+++ src/ui/note/index.tsx
@@ -1,9 +1,10 @@
-import "./styles.css";
 import { Link } from "react-router-dom";
-import editarIcon from "./icons/editar.png";
-import excluirIcon from "./icons/excluir.png";
+import "./styles.css";
 import { useState } from "react";
 import { Modal } from "../modal";
+import editIcons from "/icons/editar.png"
+import deleteIcons from "/icons/excluir.png"
+
 
 const Note = () => {
   const [showModal, setShowModal] = useState(false);
@@ -13,26 +14,28 @@ const Note = () => {
     console.log(showModal);
   };
 
-
   return (
-    <div className="container-nota">
-      <div className="dados-nota">
-        <h2>Numero de série do antivírus</h2>
-        <p>12345678</p>
-      </div>
+    <>
+      {showModal && <Modal modalChange={handleDelete} />}
+      <div className="container-nota">
+        <div className="dados-nota">
+          <h2>Numero de série do antivírus</h2>
+          <p>12345678</p>
+        </div>
 
-      <div className="container-botoes">
-        <div>s
-          <Link to="/editar">
-            <Link to='/edit'><button><img src={editarIcon} /></button></Link>
-          </Link>
+        <div className="container-botoes">
+          <div>
+            <Link to="/editar">
+              <button><img src={editIcons} /></button>
+            </Link>
 
-          <Link to="/">
-            <button onClick={handleDelete}><img src={excluirIcon} /></button>
-          </Link>
+            <Link to="/">
+              <button onClick={handleDelete}><img src={deleteIcons} /></button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
