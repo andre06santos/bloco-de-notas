16aba96f13f625ebb4d85f886cc60632ca3e547e
Thalisson Moura <thalissonmoura138@gmail.com>
2024-11-09 10:48:11 -0300

    refactor: ajustar utilização do input e button para maior flexibilidade

--- src/pages/create-page/index.tsx
+++ src/pages/create-page/index.tsx
@@ -10,16 +10,21 @@ const CreatePage = () => {
       <h1 className="titulo-pagina">Crie uma nota</h1>
 
       <form className="form">
-        <Input label="Título" />
+        <Input label="Título" typeInput="text" />
         <Textarea label="Descrição" />
 
-        <Link to="/">
-          <Button type={"button"} label={"CANCELAR"} />
-        </Link>
-
-        <Link to="/">
-          <Button type={"submit"} label={"CRIAR"} />
-        </Link>
+        <div className="container-function-page ">
+          <div className="container-button-cancel">
+            <Link to="/">
+              <Button type="button" typeButton="cancel" label={"CANCELAR"} />
+            </Link>
+          </div>
+          <div className="container-button-create">
+            <Link to="/">
+              <Input typeInput="submit" value="CRIAR" />
+            </Link>
+          </div>
+        </div>
       </form>
     </div>
   );
