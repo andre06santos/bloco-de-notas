5b11b51ac8a5fb2db26fcabd0d5d8e20147b37d8
Thalisson Moura <thalissonmoura138@gmail.com>
2024-11-09 10:46:02 -0300

    refactor: tornar as props do input mais flexíveis

--- src/ui/input/index.tsx
+++ src/ui/input/index.tsx
@@ -1,12 +1,25 @@
 import "./styles.css";
 
-const Input = ({label}: any) => {
+interface InputProps {
+  label?: string;
+  typeInput: "text" | "submit";
+  value?: string;
+}
+
+const Input: React.FC<InputProps> = ({ label, typeInput, value }) => {
+  const classInput: Record<"text" | "submit", string> = {
+    text: "input-text",
+    submit: "input-submit",
+  };
+
+  const inputClass = classInput[typeInput];
+
   return (
-    <div className={`input_component`}>
+    <div className="input_component">
       {label && <label className="input__label">{label}</label>}
-      <input type="text" />
+      <input type={typeInput} value={value} className={inputClass} />
     </div>
   );
 };
 
-export {Input};
+export { Input };
