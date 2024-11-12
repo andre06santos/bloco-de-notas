410991e156ca0255991c8edae6684313e63bfc9b
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-12 13:36:11 -0300

    :recycle: refactor: Modifica mensagens de erro e muda redirecionamento

--- src/pages/create-page/index.tsx
+++ src/pages/create-page/index.tsx
@@ -5,28 +5,41 @@ import "./styles.css";
 import { Button } from "../../ui/button";
 import { useNotes } from "../../modules/hooks/use-notes";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CreatePage = () => {
   const { createNote } = useNotes();
-  
-  const [titulo, setTitulo] = useState('')
-  const [descricao, setDescricao] = useState('')
+  const navigate = useNavigate()
 
-  const handleSubmit = async () => {
-    const note = {titulo, descricao}
-    
-      try {
-        const response = await createNote(note);
-      } catch (e) {}
-  }
+  const [titulo, setTitulo] = useState("");
+  const [descricao, setDescricao] = useState("");
+
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    const note = { titulo, descricao };    
+    try {
+      const response = await createNote(note);
+      if (!response) {
+        throw new Error("Invalid response when trying to create note");
+      }
+      console.log("Nota criada com sucesso");
+
+      navigate("/")
+      
+    } catch (error) {
+      console.error(error);
+      console.log("Ocorreu um erro ao tentar criar a nota");
+    }
+  };
 
   return (
     <div className="container-notas">
       <h1 className="titulo-pagina">Crie uma nota</h1>
 
-      <form className="form">
-        <Input label="Título" type="text" value={titulo} setValue={setTitulo}/>
-        <Textarea label="Descrição" value={descricao} setValue={setDescricao}/>
+      <form className="form" onSubmit={handleSubmit}>
+        <Input label="Título" type="text" value={titulo} onChange={(e:any) => setTitulo(e.target.value)} />
+        <Textarea label="Descrição" value={descricao} onChange={(e:any) => setDescricao(e.target.value)} />
 
         <div className="container-function-page ">
           <div className="container-button-cancel">
@@ -35,9 +48,7 @@ const CreatePage = () => {
             </Link>
           </div>
           <div className="container-button-create">
-            <Link to="/">
-              <Input type="submit" value="CRIAR" onClick={handleSubmit}/>
-            </Link>
+              <Input type="submit" value="CRIAR" />
           </div>
         </div>
       </form>
