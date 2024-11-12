51a71ae5cc0fd7176e82b6ff5f5214045ada7ce4
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-12 13:28:27 -0300

    :recycle: refactor: Retira imports e atributo desnecessario

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 import { NotesProvider } from "./modules/provider.tsx";
-import { useNotes } from "./modules/hooks/use-notes.ts";
+import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <NotesProvider value={useNotes}>
+    <NotesProvider>
       <App />
     </NotesProvider>
   </StrictMode>
