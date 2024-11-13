061015cf2864a265fb442e6a4f3426afcf81d6bf
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-13 08:25:24 -0300

    :sparkles: #60 feat: Adiciona a função de edição dentro do hook useNotes

--- src/modules/provider.tsx
+++ src/modules/provider.tsx
@@ -1,5 +1,6 @@
 import { createContext, useState, useCallback, useMemo } from "react";
 import { createNote as infraCreateNote } from "../infrastructure/create-note";
+import { editNote as infraEditNote } from "../infrastructure/edit-note";
 
 export const NotesContext = createContext<any>(undefined);
 
@@ -10,21 +11,34 @@ export const NotesProvider = ({ children }: any) => {
     try {
       const response = await infraCreateNote(note);
 
-      if (!response){
-        throw new Error("An error ocurred while trying to create the note")
+      if (!response) {
+        throw new Error("An error ocurred while trying to create the note");
       }
-      setNotes((oldNotes:any) => [...oldNotes, response])
+      setNotes((oldNotes: any) => [...oldNotes, response]);
 
       return response;
-      
-    } catch (error:any) {
+    } catch (error: any) {
       throw new Error(error.message);
-      
     }
   }, []);
-  
 
-  const value = useMemo(() => ({ notes, createNote }), [notes]);
+  const editNote = useCallback(async (note: any) => {
+    try {
+      const response = await infraEditNote(note);
+
+      if (!response) {
+        throw new Error("An error ocurred while trying to edit the note");
+      }
+
+      setNotes((oldNotes: any) => [oldNotes.map((actualNote:any) => actualNote.id !== note.id ? actualNote : note)]);
+
+      return response;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }, []);
+
+  const value = useMemo(() => ({ notes, createNote, editNote }), [notes]);
 
   return (
     <NotesContext.Provider value={value}>{children}</NotesContext.Provider>
