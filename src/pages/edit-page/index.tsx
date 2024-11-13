9f6722fe821a65e6e069c4c16d2988ddf076b471
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-13 08:26:48 -0300

    :sparkles: #60 feat: Adiciona funcionalidade de edição a sua pagina

--- src/pages/edit-page/index.tsx
+++ src/pages/edit-page/index.tsx
@@ -1,17 +1,55 @@
-import { Link } from "react-router-dom";
+import { useState, useMemo } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useNotes } from "../../modules/hooks/use-notes";
 import { Input } from "../../ui/input";
 import { Textarea } from "../../ui/textarea";
-import "./styles.css";
 import { Button } from "../../ui/button";
+import "./styles.css";
 
 const EditPage = () => {
+  const { editNote } = useNotes();
+
+  const { state } = useLocation();
+  const navigate = useNavigate();
+  const id = state.id;
+
+  const [titulo, setTitulo] = useState(state.titulo);
+  const [descricao, setDescricao] = useState(state.descricao);
+
+  const note = {
+    id,
+    titulo,
+    descricao,
+  };
+
+  const handleSumbit = async (e:any) => {
+    e.preventDefault()
+    try{
+
+      const response = await editNote(note);
+      
+      if (!response) {
+        throw new Error("Invalid response when trying to edit note");
+      }
+      
+      console.log("Nota editada com sucesso");
+      navigate("/")
+      
+      return response;
+    } catch (error){
+      console.error(error)
+      console.log("Ocorreu um erro ao tentar editar a nota");
+      
+    }
+  };
+
   return (
     <div className="container-notas">
       <h1 className="titulo-pagina">Editar a nota</h1>
 
-      <form action="">
-        <Input label="Título" type="text" />
-        <Textarea label="Descrição" />
+      <form action="" onSubmit={handleSumbit}>
+        <Input label="Título" type="text" value={titulo} onChange={setTitulo} />
+        <Textarea label="Descrição" value={descricao} onChange={setDescricao} />
 
         <div className="container-function-page ">
           <div className="container-button-cancel">
@@ -20,9 +58,7 @@ const EditPage = () => {
             </Link>
           </div>
           <div className="container-button-save">
-            <Link to="/">
-              <Input type="submit" value="SALVAR" />
-            </Link>
+            <Input type="submit" value="SALVAR" />
           </div>
         </div>
       </form>
