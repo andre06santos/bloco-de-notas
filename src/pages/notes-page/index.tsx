8c9648c915d5735c3181ad1c9c996a5bdb4a42c6
Thalisson Moura <thalissonmoura138@gmail.com>
2024-11-14 09:33:05 -0300

    refactor: remove código desnecessário do get-notes

--- src/pages/notes-page/index.tsx
+++ src/pages/notes-page/index.tsx
@@ -1,33 +1,14 @@
 import "./styles.css";
 import { Note } from "../../ui/note";
-import { useEffect } from "react";
 import { useNotes } from "../../modules/hooks/use-notes";
 
 const NotesPage = () => {
-  const { notes, getNotes } = useNotes();
-
-  useEffect(() => {
-    const fetchNotes = async () => {
-      try {
-        if (notes.length === 0) {
-          await getNotes();
-        }
-      } catch (error) {
-        console.error("Erro ao tentar buscar as notas:", error);
-      }
-    };
-
-    fetchNotes();
-  }, [getNotes]);
-
-  if (notes.length === 0) {
-    return <div className="container-notes-page">Nenhuma nota encontrada.</div>;
-  }
+  const { notes } = useNotes();
 
   return (
     <div className="container-notes-page">
       {notes.map((note: any) => (
-        <Note key={note.id} note={note} />
+        <Note key={note.id} id={note.id} titulo={note.titulo} descricao={note.descricao}/>
       ))}
     </div>
   );
