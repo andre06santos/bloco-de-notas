149494938c8b718ad8df1509a67e5d6fe9857dc9
Thalisson Moura <thalissonmoura138@gmail.com>
2024-11-17 10:15:26 -0300

    feat: adicionar tipagem para as props do componente Input

--- src/ui/input/index.tsx
+++ src/ui/input/index.tsx
@@ -2,15 +2,15 @@ import { InputHTMLAttributes } from "react";
 import "./styles.css";
 
 type InputProps = {
-  label: string,
-  type: "text" | "submit" | "reset",
-} | InputHTMLAttributes<HTMLInputElement>
+  label?: string;
+  type: "text" | "submit" | "reset";
+} & InputHTMLAttributes<HTMLInputElement>;
 
-const Input = ({ label, type , ...rest}: InputProps) => {
+const Input = ({ label, type, ...rest }: InputProps) => {
   const inputClasses: Record<"text" | "submit" | "reset", string> = {
     text: "input-text",
     submit: "input-submit",
-    reset: "input-reset"
+    reset: "input-reset",
   };
 
   const className = inputClasses[type];
@@ -18,7 +18,7 @@ const Input = ({ label, type , ...rest}: InputProps) => {
   return (
     <div className="input_component">
       {label && <label className="input__label">{label}</label>}
-      <input type={type} className={className} {...rest}/>
+      <input type={type} className={className} {...rest} />
     </div>
   );
 };
